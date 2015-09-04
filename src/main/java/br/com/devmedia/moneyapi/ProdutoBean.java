7ebb642cdc61be1082f1da48da2be0c2adda6b83
alexandregama <alexandre.gama.lima@gmail.com>
2015-09-04 12:33:54 -0300

    Cria metodo salva e getLista para salvar e listar os produtos

--- src/main/java/br/com/devmedia/moneyapi/ProdutoBean.java
+++ src/main/java/br/com/devmedia/moneyapi/ProdutoBean.java
@@ -1,11 +1,33 @@
 package br.com.devmedia.moneyapi;
 
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class ProdutoBean {
 
 	private Produto produto = new Produto();
+	
+	private Produtos produtos = new ProdutosEmMemoria();
+	
+	private List<Produto> lista;
+	
+	public void salva() {
+		produtos.cadastra(produto);
+		
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cadastro com sucesso"));
+		this.produto = new Produto();
+	}
+	
+	public List<Produto> getLista() {
+		if (lista == null) {
+			lista = produtos.getTodosOsProdutos();
+		}
+		return lista;
+	}
 
 	public Produto getProduto() {
 		return produto;
