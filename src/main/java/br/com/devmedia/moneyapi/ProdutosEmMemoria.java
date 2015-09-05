3a65740436cf2188c0358f676b42743c2e5eb907
alexandregama <alexandre.gama.lima@gmail.com>
2015-09-05 11:44:27 -0300

    Implementa incremento do id para simular o banco de dados

--- src/main/java/br/com/devmedia/moneyapi/ProdutosEmMemoria.java
+++ src/main/java/br/com/devmedia/moneyapi/ProdutosEmMemoria.java
@@ -1,18 +1,24 @@
 package br.com.devmedia.moneyapi;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ProdutosEmMemoria implements Produtos {
+import javax.enterprise.context.ApplicationScoped;
 
-	private Map<Long, Produto> banco = new HashMap<>();
+@ApplicationScoped
+public class ProdutosEmMemoria implements Produtos, Serializable {
+
+	private static final long serialVersionUID = -8272280326281942350L;
+	
+	private static final Map<Long, Produto> banco = new HashMap<>();
 	private Long id = 1l;
 	
 	@Override
 	public void cadastra(Produto produto) {
-		produto.setId(id);
+		produto.setId(++id);
 		banco.put(id, produto);
 	}
 
