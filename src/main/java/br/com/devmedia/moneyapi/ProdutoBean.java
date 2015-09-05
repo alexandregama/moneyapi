9578fe19c6969e27ab7028445e651803de50c02a
alexandregama <alexandre.gama.lima@gmail.com>
2015-09-05 11:43:56 -0300

    Passa a usar o @Named do CDI para possibilitar a injeção de dependências

--- src/main/java/br/com/devmedia/moneyapi/ProdutoBean.java
+++ src/main/java/br/com/devmedia/moneyapi/ProdutoBean.java
@@ -2,24 +2,36 @@ package br.com.devmedia.moneyapi;
 
 import java.util.List;
 
+import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
 
-@ManagedBean
+@Named
+@RequestScoped
 public class ProdutoBean {
 
 	private Produto produto = new Produto();
 	
-	private Produtos produtos = new ProdutosEmMemoria();
+	private Produtos produtos;
 	
 	private List<Produto> lista;
+	
+	@Inject
+	public ProdutoBean(Produtos produtos) {
+		this.produtos = produtos;
+	}
+	
+	public ProdutoBean() {
+	}
 	
 	public void salva() {
 		produtos.cadastra(produto);
 		
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cadastro com sucesso"));
 		this.produto = new Produto();
+		this.lista = null;
 	}
 	
 	public List<Produto> getLista() {
