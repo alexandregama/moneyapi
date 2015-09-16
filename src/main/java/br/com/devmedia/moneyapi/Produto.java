5148d16ece2461e97faadc9d297a278d1603bf45
alexandregama <alexandre.gama.lima@gmail.com>
2015-09-16 10:08:30 -0300

    Formata o preco com o CurrencyCode

--- src/main/java/br/com/devmedia/moneyapi/Produto.java
+++ src/main/java/br/com/devmedia/moneyapi/Produto.java
@@ -1,8 +1,11 @@
 package br.com.devmedia.moneyapi;
 
 import java.io.Serializable;
+import java.util.Locale;
 
 import javax.money.MonetaryAmount;
+import javax.money.format.MonetaryAmountFormat;
+import javax.money.format.MonetaryFormats;
 
 public class Produto implements Serializable {
 
@@ -16,10 +19,21 @@ public class Produto implements Serializable {
 	
 	private MonetaryAmount preco;
 	
+	private String precoFormatadoComCode;
+	
 	@Override
 	public String toString() {
 		return "Produto [id=" + id + ", nome=" + nome + ", descricao="
 				+ descricao + ", preco=" + preco + "]";
+	}
+	
+	public String getPrecoFormatadoComCode() {
+		if (preco != null) {
+			MonetaryAmountFormat formatador = MonetaryFormats.getAmountFormat(new Locale("pt", "BR"));
+			precoFormatadoComCode = formatador.format(preco);
+		}
+		
+		return precoFormatadoComCode;
 	}
 	
 	public String getNome() {
